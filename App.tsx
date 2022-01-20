83d7ca2bb306135a55bc2642dff6ed0b73867034
Lawrence <tech@l-z.fr>
2022-01-20 18:32:57 +0100

    feat: first attemp

--- App.tsx
+++ App.tsx
@@ -1,71 +1,126 @@
-import React from "react";
-import {
-  Text,
-  Link,
-  HStack,
-  Center,
-  Heading,
-  Switch,
-  useColorMode,
-  NativeBaseProvider,
-  extendTheme,
-  VStack,
-  Code,
-} from "native-base";
-import NativeBaseIcon from "./components/NativeBaseIcon";
+import * as React from "react";
+import "react-native-gesture-handler";
 
-// Define the config
-const config = {
-  useSystemColorMode: false,
-  initialColorMode: "dark",
+import { NavigationContainer } from "@react-navigation/native";
+
+import {
+  createDrawerNavigator,
+  DrawerContentComponentProps,
+  DrawerContentScrollView,
+} from "@react-navigation/drawer";
+
+import { Ionicons } from "@expo/vector-icons";
+
+import {
+  NativeBaseProvider,
+  Box,
+  Pressable,
+  VStack,
+  Text,
+  Center,
+  HStack,
+  Divider,
+  Icon,
+} from "native-base";
+
+const Drawer = createDrawerNavigator();
+
+const DefaultContent: React.FC<{ route: { name: string } }> = (props) => {
+  return (
+    <Center>
+      <Text mt="12" fontSize="18">
+        This is {props.route.name} page.
+      </Text>
+    </Center>
+  );
 };
 
-// extend the theme
-export const theme = extendTheme({ config });
+const getIcon = (screenName: string) => {
+  switch (screenName) {
+    case "Inbox":
+      return <Ionicons name="person" />;
+    case "Outbox":
+      return <Ionicons name="person" />;
+    case "Favorites":
+      return <Ionicons name="person" />;
+    default:
+      return <Ionicons />;
+  }
+};
 
-export default function App() {
+const CustomDrawerContent = (props: DrawerContentComponentProps) => {
   return (
-    <NativeBaseProvider>
-      <Center
-        _dark={{ bg: "blueGray.900" }}
-        _light={{ bg: "blueGray.50" }}
-        px={4}
-        flex={1}
-      >
-        <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <HStack space={2} alignItems="center">
-            <Text>Edit</Text>
-            <Code>App.tsx</Code>
-            <Text>and save to reload.</Text>
-          </HStack>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={"xl"}>
-              Learn NativeBase
-            </Text>
-          </Link>
-          <ToggleDarkMode />
+    <DrawerContentScrollView {...props}>
+      <VStack space="6" my="2" mx="1">
+        <Box px="4">
+          <Text bold color="gray.700">
+            Mail
+          </Text>
+          <Text fontSize="14" mt="1" color="gray.500" fontWeight="500">
+            john_doe@gmail.com
+          </Text>
+        </Box>
+        <VStack divider={<Divider />} space="4">
+          <VStack space="3">
+            {props.state.routeNames.map((name, index) => (
+              <Pressable
+                px="5"
+                py="3"
+                rounded="md"
+                bg={
+                  index === props.state.index
+                    ? "rgba(6, 182, 212, 0.1)"
+                    : "transparent"
+                }
+                onPress={(event) => {
+                  props.navigation.navigate(name);
+                }}
+              >
+                <HStack space="7" alignItems="center">
+                  <Icon
+                    color={
+                      index === props.state.index ? "primary.500" : "gray.500"
+                    }
+                    size="5"
+                    as={getIcon(name)}
+                  />
+                  <Text
+                    fontWeight="500"
+                    color={
+                      index === props.state.index ? "primary.500" : "gray.700"
+                    }
+                  >
+                    {name}
+                  </Text>
+                </HStack>
+              </Pressable>
+            ))}
+          </VStack>
         </VStack>
-      </Center>
-    </NativeBaseProvider>
+      </VStack>
+    </DrawerContentScrollView>
   );
-}
-
-// Color Switch Component
-function ToggleDarkMode() {
-  const { colorMode, toggleColorMode } = useColorMode();
+};
+const MenuDrawer = () => {
   return (
-    <HStack space={2} alignItems="center">
-      <Text>Dark</Text>
-      <Switch
-        isChecked={colorMode === "light" ? true : false}
-        onToggle={toggleColorMode}
-        aria-label={
-          colorMode === "light" ? "switch to dark mode" : "switch to light mode"
-        }
-      />
-      <Text>Light</Text>
-    </HStack>
+    <Box safeArea flex={1}>
+      <Drawer.Navigator
+        drawerContent={(props) => <CustomDrawerContent {...props} />}
+      >
+        <Drawer.Screen name="Inbox" component={DefaultContent} />
+      </Drawer.Navigator>
+    </Box>
   );
-}
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <NativeBaseProvider>
+        <MenuDrawer />
+      </NativeBaseProvider>
+    </NavigationContainer>
+  );
+};
+
+export default App;
