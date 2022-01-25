1799cb98c4c0a949b939aef1afe1597213eac8c0
Lawrence <tech@l-z.fr>
2022-01-25 19:11:51 +0100

    feat: working on visual identity

---
+++ icons/Logo.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import Svg, { G, Path, Defs, Stop, SvgProps } from 'react-native-svg';
+
+const Lupulus: React.FC<SvgProps> = ({ width = '396', height = '352' }) => {
+  return (
+    <Svg viewBox="100 0 3360 3700" width={width} height={height} transform={{ scale: [1.3, 1.3] }}>
+      <Defs>
+        <linearGradient id="lg3IMchcjVqrrtbi" gradientUnits="userSpaceOnUse" x1="47.979797%" y1="42.897728%" x2="47.979797%" y2="82.10227%">
+          <Stop offset="0%" stopColor="rgb(105,203,49)"></Stop>
+          <Stop offset="100%" stopColor="rgb(63,119,30)"></Stop>
+        </linearGradient>
+        <linearGradient id="lg1EPINu3xWEOcdc" gradientUnits="userSpaceOnUse" x1="42.17172%" y1="14.204545%" x2="42.17172%" y2="89.48864%">
+          <Stop offset="0%" stopColor="rgb(97,102,103)"></Stop>
+          <Stop offset="7%" stopColor="rgb(91,96,99)"></Stop>
+          <Stop offset="8%" stopColor="rgb(91,97,99)"></Stop>
+          <Stop offset="48%" stopColor="rgb(54,58,61)"></Stop>
+          <Stop offset="54%" stopColor="rgb(49,50,54)"></Stop>
+          <Stop offset="55%" stopColor="rgb(48,50,53)"></Stop>
+          <Stop offset="57%" stopColor="rgb(46,47,51)"></Stop>
+          <Stop offset="72%" stopColor="rgb(33,35,38)"></Stop>
+          <Stop offset="76%" stopColor="rgb(29,30,34)"></Stop>
+          <Stop offset="77%" stopColor="rgb(28,30,33)"></Stop>
+          <Stop offset="79%" stopColor="rgb(27,28,31)"></Stop>
+          <Stop offset="81%" stopColor="rgb(28,30,32)"></Stop>
+          <Stop offset="84%" stopColor="rgb(27,29,31)"></Stop>
+          <Stop offset="85%" stopColor="rgb(27,28,31)"></Stop>
+          <Stop offset="86%" stopColor="rgb(27,29,31)"></Stop>
+          <Stop offset="96%" stopColor="rgb(28,30,33)"></Stop>
+          <Stop offset="98%" stopColor="rgb(27,29,31)"></Stop>
+          <Stop offset="99%" stopColor="rgb(29,31,31)"></Stop>
+          <Stop offset="100%" stopColor="rgb(29,31,31)"></Stop>
+        </linearGradient>
+      </Defs>
+      <G fill="rgb(58,61,64)">
+        <G>
+          <Path
+            d="M2199 3128 c237 -379 339 -648 382 -1011 17 -148 15 -381 -5 -456 -37 -145 -97 -230 -195 -280 -53 -28 -149 -28 -209 -1 -54 24 -155 120 -197 185 -55 87 -76 147 -98 273 -12 74 -4 73 -140 17 -183 -74 -286 -83 -392 -30 -67 32 -97 79 -121 184 -14 62 -15 83 -4 149 33 210 104 339 298 543 88 93 229 196 375 274 l82 45 -65 0 c-183 0 -420 -111 -584 -273 -54 -55 -169 -200 -221 -279 -9 -16 -21 -28 -26 -28 -5 0 -20 24 -34 54 -32 68 -96 141 -146 166 -50 26 -123 26 -192 0 -72 -27 -207 -121 -207 -145 0 -3 11 -5 25 -5 14 0 25 -2 25 -5 0 -3 -25 -30 -55 -60 -63 -63 -70 -84 -25 -77 35 5 35 6 11 -50 -10 -24 -22 -67 -26 -95 -7 -54 2 -183 14 -183 3 0 28 46 56 102 122 252 252 355 361 287 24 -15 45 -43 72 -97 40 -83 38 -69 33 -297 -2 -116 11 -183 54 -273 67 -141 194 -233 344 -248 92 -10 214 24 323 90 l57 34 6 -67 c16 -164 68 -279 182 -400 76 -82 160 -148 244 -193 49 -25 68 -43 69 -60 0 -5 -30 -8 -67 -8 -67 -1 -140 -24 -136 -44 1 -5 42 -19 91 -32 49 -12 109 -32 133 -44 62 -32 179 -114 214 -150 25 -26 36 -30 85 -30 l55 0 80 -80 c89 -89 99 -90 108 -7 4 39 -1 70 -19 125 -13 40 -24 75 -24 78 0 3 28 2 61 -2 70 -8 70 -13 0 80 -146 193 -154 395 -22 560 17 21 31 40 31 43 0 2 -11 1 -25 -3 -17 -4 -25 -2 -25 7 0 23 39 95 65 119 l24 23 -19 25 c-25 31 -25 39 3 95 l24 46 -21 3 c-13 2 -19 9 -17 18 2 8 5 78 6 155 5 245 -43 465 -143 671 -87 176 -229 380 -344 491 -56 55 -212 163 -234 163 -8 0 11 -39 45 -92z m378 -2405 c38 -18 53 -41 33 -53 -19 -12 -43 -2 -86 36 l-39 34 28 0 c16 0 45 -7 64 -17z"
+            fill="url(#lg1EPINu3xWEOcdc)"></Path>
+        </G>
+      </G>
+      <G id="l5csvTFS21dU2km6n2S1gRk" fill="rgb(152,4,0)">
+        <G>
+          <Path
+            d="M2038 2910 c-178 -60 -283 -125 -410 -252 -222 -223 -325 -408 -316 -567 3 -56 9 -75 31 -105 49 -63 69 -71 182 -71 91 1 107 4 180 35 44 19 114 57 156 86 l76 52 7 -89 c19 -254 145 -481 288 -520 119 -32 221 92 253 307 19 124 19 164 0 343 -29 269 -97 467 -248 723 -61 104 -63 104 -199 58z"
+            fill="url(#lg3IMchcjVqrrtbi)"></Path>
+        </G>
+      </G>
+    </Svg>
+  );
+};
+
+export default Lupulus;
--- screens/AuthSignUp.tsx
+++ screens/AuthSignUp.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import { Button, Center, Input, useSafeArea, VStack } from 'native-base';
+import { Button, Center, HStack, Input, Text, useSafeArea, VStack } from 'native-base';
 import { useTranslation } from 'react-i18next';
 import { authSignUp } from '../redux/authActions';
 import { Auth, useAppDispatch, useAppSelector } from '../redux/types';
+import Logo from '../icons/Logo2';
 
 const Screen: React.FC<{ navigation: any }> = props => {
   const { t } = useTranslation();
@@ -21,17 +22,23 @@ const Screen: React.FC<{ navigation: any }> = props => {
 
   return (
     <Center minH="100%" w="100%" {...safeAreaProps}>
-      <Center flex={1} px={3} w="100%">
-        <VStack space={2} w={{ base: '100%', sm: '200px' }}>
-          <Input type="text" placeholder={t('signup.your-email')} onChangeText={(value: string) => setEmail(value)} />
-          <Input type="password" placeholder={t('signup.password')} onChangeText={(value: string) => setPassword(value)} />
-          <Button
-            onPress={() => {
-              dispatch(authSignUp(email, password));
-            }}>
-            {t('sign-up')}
-          </Button>
-        </VStack>
+      <Center flex={1} px={8} w="100%">
+        <HStack>
+          <Center>
+            <Logo width="128" height="256" />
+            <Text fontSize="md">SemperLupulus</Text>
+          </Center>
+          <VStack space={{ base: 4, sm: 2 }} w={{ base: '100%', sm: '200px' }}>
+            <Input type="text" placeholder={t('signup.your-email')} onChangeText={(value: string) => setEmail(value)} />
+            <Input type="password" placeholder={t('signup.password')} onChangeText={(value: string) => setPassword(value)} />
+            <Button
+              onPress={() => {
+                dispatch(authSignUp(email, password));
+              }}>
+              {t('sign-up')}
+            </Button>
+          </VStack>
+        </HStack>
       </Center>
     </Center>
   );
